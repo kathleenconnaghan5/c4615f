7c5ec80ca6f5bdf30aa34af6029de1a7b1893705
kathleenconnaghan <77138134+kathleenconnaghan@users.noreply.github.com>
2022-01-08 16:53:18 -0600

    adds user-convo table to db, refactor db models

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -4,39 +4,49 @@ const Message = require("./message");
 const Sequelize = require("sequelize");
 
 const Conversation = db.define("conversation", {
-  user1LastMessageIdSeen: {
-    type: Sequelize.INTEGER,
-    allowNull: true,
-  },
-  user2LastMessageIdSeen: {
-    type: Sequelize.INTEGER,
-    allowNull: true,
-  },
-  user1UnreadMsgCount: {
-    type: Sequelize.INTEGER,
-    defaultValue: 0,
-    allowNull: false,
-  },
-  user2UnreadMsgCount: {
-    type: Sequelize.INTEGER,
-    defaultValue: 0,
-    allowNull: false,
-  },
+
+  // No longer needed, the equivalent values are now stores in UserConversation
+
+  // user1LastMessageIdSeen: {
+  //   type: Sequelize.INTEGER,
+  //   allowNull: true,
+  // },
+  // user2LastMessageIdSeen: {
+  //   type: Sequelize.INTEGER,
+  //   allowNull: true,
+  // },
+  // user1UnreadMsgCount: {
+  //   type: Sequelize.INTEGER,
+  //   defaultValue: 0,
+  //   allowNull: false,
+  // },
+  // user2UnreadMsgCount: {
+  //   type: Sequelize.INTEGER,
+  //   defaultValue: 0,
+  //   allowNull: false,
+  // },
 });
 
 // find conversation given two user Ids
 
-Conversation.findConversation = async function (user1Id, user2Id) {
-  const conversation = await Conversation.findOne({
+// Conversation.findConversation = async function (user1Id, user2Id) {
+//   const conversation = await Conversation.findOne({
+//     where: {
+//       user1Id: {
+//         [Op.or]: [user1Id, user2Id]
+//       },
+//       user2Id: {
+//         [Op.or]: [user1Id, user2Id]
+//       }
+//     }
+//   });
+
+Conversation.findConversation = async function (conversationId) {
+  const conversation = await Conversation.findAll({
     where: {
-      user1Id: {
-        [Op.or]: [user1Id, user2Id]
-      },
-      user2Id: {
-        [Op.or]: [user1Id, user2Id]
-      }
+      conversationId: conversationId,
     }
-  });
+  })
 
   // return conversation or null if it doesn't exist
   return conversation;
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,17 +1,23 @@
 const Conversation = require("./conversation");
+const UserConversation = require("./user-conversation");
 const User = require("./user");
 const Message = require("./message");
 
 // associations
 
 User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+// Conversation.belongsTo(User, { as: "user1" });
+// Conversation.belongsTo(User, { as: "user2" });
+UserConversation.belongsTo(User, { as: "userId" } );
+UserConversation.belongsTo(Conversation, { as: "conversationId" } )
+Conversation.hasMany(UserConversation);
+User.hasMany(UserConversation);
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 
 module.exports = {
   User,
   Conversation,
-  Message
+  Message,
+  UserConversation
 };
